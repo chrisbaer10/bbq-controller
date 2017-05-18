a85bbf6fed95e8eb95b60baf335c9ebb5141e557
Chris Baer <chrisbaer10@gmail.com>
2017-05-17 22:34:46 -0500

    adding graphing

--- bbq_controller.py
+++ bbq_controller.py
@@ -1,23 +1,77 @@
 #!/usr/bin/python
 
+import sys
 from time import sleep
+import datetime
 import argparse
 from w1thermsensor import W1ThermSensor
 import ConfigParser
 import collections
 import RPi.GPIO as GPIO
+import plotly.plotly as py
+import plotly.tools as tls
+import plotly.graph_objs as go
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
 parser.add_argument('-q', '--quiet', dest='quiet', action='store_true')
 args = parser.parse_args()
 
+def setup_stream():
+	log_verbose("Setting up Stream...")
+	filename='bbq_streaming'
+	title='BBQ Streaming'
+	stream_tokens = tls.get_credentials_file()['stream_ids']
+	token_1 = stream_tokens[-1]
+	log_verbose("Pit Temp Stream Token " + token_1)
+	token_2 = stream_tokens[-2]
+	log_verbose("Pit Temp Stream Token " + token_2)
+	token_3 = stream_tokens[-3]
+	log_verbose("Motor Stream Token " + token_3)
+	stream_id1 = dict(token=token_1, maxpoints=60)
+	stream_id2 = dict(token=token_2, maxpoints=60)
+	stream_id3 = dict(token=token_3, maxpoints=60)
+	trace1 = go.Scatter(x=[], y=[], stream=stream_id1, name='Pit Temp')
+	trace2 = go.Scatter(x=[], y=[], stream=stream_id2, name='Meat Temp')
+	trace3 = go.Scatter(x=[], y=[], stream=stream_id3, name='Motor Percent')
+	data = [trace1, trace2, trace3]
+	layout = dict(title = title,
+		xaxis = dict(title = 'Date'),
+		yaxis = dict(title = 'Temperature (degrees F)'),
+		shapes=[{
+			'type': 'line',
+			'xref': 'paper',
+			'x0': 0,
+			'y0': c.set_temp,
+			'x1': 1,
+			'y1': c.set_temp, # ditto
+			'line': {
+				'color': 'rgb(185, 52, 52)',
+                                'width': 1,
+                                'dash': 'dash',
+			},
+		}]
+	)
+	fig = go.Figure(data=data, layout=layout)
+	plot_url = py.plot(fig, filename=filename)
+	global s_1
+	s_1 = py.Stream(stream_id=token_1)
+	global s_2
+	s_2 = py.Stream(stream_id=token_2)
+	global s_3
+	s_3 = py.Stream(stream_id=token_3)
+	s_1.open()
+	s_2.open()
+	s_3.open()
+	log_verbose("Done Setting up Stream\n")
+
 def read_config():
 	config = ConfigParser.RawConfigParser()
 	config.read('bbq_controller.cfg')
 	con = collections.namedtuple('config', ['pit', 'meat', 'set_temp'])
+	global c
 	c = con(config.get('Sensors', 'pit_sensorid'), config.get('Sensors', 'meat_sensorid'), config.get('Configuration', 'set_temp'))
-	return c
 
 def get_temp( sensor_id ):
 	sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, sensor_id)
@@ -28,7 +82,7 @@ def log_verbose( string ):
 		print string
 	return
 
-def main():
+def setup_motor():
 	GPIO.setmode(GPIO.BOARD)
 	Motor1A = 16
 	Motor1B = 18
@@ -41,9 +95,14 @@ def main():
 	GPIO.output(Motor1B,GPIO.LOW)
 	GPIO.output(Motor1E,GPIO.HIGH)
 
+	global p
 	p = GPIO.PWM(Motor1A, 50)
 	p.start(0)
 
+def main():
+	read_config()
+	setup_stream()
+	setup_motor()
 	P = 5
 	I = .1
 	D = 10
@@ -53,19 +112,23 @@ def main():
 	accumulatedError = 0
 	sum = 0
 
-	config = read_config()
-	print ("Pit Temperature Sensor: " + str(config.pit))
-	print ("Meat Temperature Sensor: " + str(config.meat))
+	print ("Pit Temperature Sensor: " + str(c.pit))
+	print ("Meat Temperature Sensor: " + str(c.meat))
 	log_verbose("Starting Program...")
-	print ("Set Temperature: " + str(config.set_temp))
+	print ("Set Temperature: " + str(c.set_temp))
 	print
 	try:
 		while True:
-			print ("Current Pit Temperature: " + str(get_temp(config.pit)))
-			print ("Current Meat Temperature: " + str(get_temp(config.meat)))
+			date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+			pit_temp = str(get_temp(c.pit))
+			meat_temp = str(get_temp(c.meat))
+			print ("Current Pit Temperature: " + pit_temp)
+			print ("Current Meat Temperature: " + meat_temp)
 			print
-                        current_temp = int(get_temp(config.pit))
-			target_temp = int(config.set_temp)
+			s_1.write(dict(x=date,y=pit_temp))
+			s_2.write(dict(x=date,y=meat_temp))
+                        current_temp = int(get_temp(c.pit))
+			target_temp = int(c.set_temp)
                         error = (target_temp) - (current_temp)
 			log_verbose("Error: " + str(error))
 			if 0 < fanSpeed and fanSpeed < 100:
@@ -77,19 +140,24 @@ def main():
 			log_verbose("Average Temp: %d" % averageTemp)
 			fanSpeed = B + ( P * error ) + ( I * accumulatedError ) + ( D * (averageTemp - current_temp)) 
 			if fanSpeed <= 0:
+				s_3.write(dict(x=date,y=0))
 				p.ChangeDutyCycle(0)
 			elif fanSpeed >= 100: 
+				s_3.write(dict(x=date,y=100))
 				p.ChangeDutyCycle(100)
 			else:
+				s_3.write(dict(x=date,y=fanSpeed))
 				p.ChangeDutyCycle(fanSpeed)
 			print "Fan Speed: %d" % fanSpeed
 	except KeyboardInterrupt:
 		print 'interrupted!'
 		p.stop()
 		GPIO.cleanup()
-	except: 
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
 		p.stop()
 		GPIO.cleanup()
+		raise
 		
 
 if __name__ == "__main__":
---
+++ init_plotly.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+import plotly 
+plotly.tools.set_credentials_file(username='your_username', api_key='your_api_key')
